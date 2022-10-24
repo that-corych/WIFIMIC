5935867122c959ccc731131a531e523523905691
shest <shest@DESKTOP-B4J5JAE>
2022-10-25 00:12:42 +0300

    Добавьте файлы проекта.

---
+++ WIFIMIC/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace WIFIMIC
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonHide_Click(object sender, RoutedEventArgs e)
+        {
+            tba.ShowBalloonTip("I'm Here", "", BalloonIcon.Warning);
+        }
+    }
+}
